ea81624c951e9968765a3e348a543dcec940a909
touchportal <minecraftpaintballer@gmail.com>
2020-02-25 19:16:21 +0800

    v0.3-alpha

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
     $(".notes").hover(
     function(){
-        $("#header-mount").css("background", "url('img/header-notes.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-notes.png') fixed top center");
         $("#header-mount").css("background-size", "contain");
     },
     function(){
-        $("#header-mount").css("background", "url('img/header-default.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-default.png') fixed top center");
     });
     
     $(".writing").hover(
     function(){
-        $("#header-mount").css("background", "url('img/header-writing.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-writing.png') fixed top center");
         $("#header-mount").css("background-size", "contain");
     },
     function(){
-        $("#header-mount").css("background", "url('img/header-default.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-default.png') fixed top center");
     });
     
     $(".images").hover(
     function(){
-        $("#header-mount").css("background", "url('img/header-images.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-images.png') fixed top center");
         $("#header-mount").css("background-size", "contain");
     },
     function(){
-        $("#header-mount").css("background", "url('img/header-default.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-default.png') fixed top center");
     });
     
     $(".others").hover(
     function(){
-        $("#header-mount").css("background", "url('img/header-others.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-others.png') fixed top center");
         $("#header-mount").css("background-size", "contain");
     },
     function(){
-        $("#header-mount").css("background", "url('img/header-default.png') fixed top center");
+        $("#header-mount").css("background", "url('../img/header-default.png') fixed top center");
     });
     
     $.getJSON( "content/post-12-01-2020.json", function( handler ) {
